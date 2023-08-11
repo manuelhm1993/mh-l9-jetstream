4264b6470512e355f82ba1cafbd85477629109d9
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-11 04:59:08 -0400

    Navegar de home a admin
    Se creó el link para navegar desde el manejador de cuentas al panel administrativo y del panel administrativo a home

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -7,6 +7,19 @@
             'active' => request()->routeIs('home')
         ],
     ];
+
+    $account_links = [
+        [
+            'name'   => 'Profile',
+            'route'  => route('profile.show'),
+            'active' => request()->routeIs('profile.show')
+        ],
+        [
+            'name'   => 'Panel admin',
+            'route'  => route('dashboard'),
+            'active' => request()->routeIs('dashboard')
+        ],
+    ];
 @endphp
 
 <nav x-data="{ open: false }" class="bg-white border-b border-gray-100 shadow">
@@ -87,67 +100,70 @@
 
                 <!-- Settings Dropdown -->
                 <div class="ml-3 relative">
-                    @auth
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            <div class="flex items-center px-4">
-                                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                    <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                        <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                    </button>
-                                @endif
-                                
-                                <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
-                                        {{ Auth::user()->name }}
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                        </svg>
-                                    </button>
-                                </span>
-                            </div>
-                        </x-slot>
-
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
-
-                            <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-
-                            @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
-                                <x-dropdown-link href="{{ route('api-tokens.index') }}">
-                                    {{ __('API Tokens') }}
-                                </x-dropdown-link>
+                @auth
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <div class="flex items-center px-4">
+                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                </button>
                             @endif
+                            
+                            <span class="inline-flex rounded-md">
+                                <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none focus:bg-gray-50 active:bg-gray-50 transition ease-in-out duration-150">
+                                    {{ Auth::user()->name }}
+                                    <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                    </svg>
+                                </button>
+                            </span>
+                        </div>
+                    </x-slot>
 
-                            <div class="border-t border-gray-200"></div>
+                    <x-slot name="content">
+                        <!-- Account Management -->
+                        <div class="block px-4 py-2 text-xs text-gray-400">
+                            {{ __('Manage Account') }}
+                        </div>
 
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}" x-data>
-                                @csrf
+                        {{-- Links para la cuenta de usuario --}}
+                        @foreach ($account_links as $account_link)
+                        <x-dropdown-link href="{{ $account_link['route'] }}">
+                            {{ __($account_link['name']) }}
+                        </x-dropdown-link>
+                        @endforeach
 
-                                <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
-                    @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700">
-                            {{ __('Log in') }}
-                        </a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700">
-                                {{ __('Register') }}
-                            </a>
+                        @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
+                            <x-dropdown-link href="{{ route('api-tokens.index') }}">
+                                {{ __('API Tokens') }}
+                            </x-dropdown-link>
                         @endif
-                    @endauth
+
+                        <div class="border-t border-gray-200"></div>
+
+                        <!-- Authentication -->
+                        <form method="POST" action="{{ route('logout') }}" x-data>
+                            @csrf
+
+                            <x-dropdown-link href="{{ route('logout') }}"
+                                        @click.prevent="$root.submit();">
+                                {{ __('Log Out') }}
+                            </x-dropdown-link>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700">
+                        {{ __('Log in') }}
+                    </a>
+
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700">
+                            {{ __('Register') }}
+                        </a>
+                    @endif
+                @endauth
                 </div>
             </div>
 
@@ -191,9 +207,12 @@
 
             <div class="mt-3 space-y-1">
                 <!-- Account Management -->
-                <x-responsive-nav-link href="{{ route('profile.show') }}" :active="request()->routeIs('profile.show')">
-                    {{ __('Profile') }}
+                {{-- Links para la cuenta de usuario --}}
+                @foreach ($account_links as $account_link)
+                <x-responsive-nav-link href="{{ $account_link['route'] }}" :active="$account_link['active']">
+                    {{ __($account_link['name']) }}
                 </x-responsive-nav-link>
+                @endforeach
 
                 @if (Laravel\Jetstream\Jetstream::hasApiFeatures())
                     <x-responsive-nav-link href="{{ route('api-tokens.index') }}" :active="request()->routeIs('api-tokens.index')">
