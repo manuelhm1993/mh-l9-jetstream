8a53dae252e1715d405e0f0d03182866f1cab768
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-11 01:22:03 -0400

    Personalización jetstream iniciada
    Se eliminó el slot con nombre header en el componente anónimo dashboard y por consiguiente, se eliminó su llamada en el componente app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,10 +10,6 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-
         <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 
         <!-- Styles -->
@@ -24,15 +20,6 @@
 
         <div class="min-h-screen bg-gray-100">
             @livewire('navigation-menu')
-
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
 
             <!-- Page Content -->
             <main>
